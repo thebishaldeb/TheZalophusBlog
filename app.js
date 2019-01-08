6e15e047179733194c3fbbed6ceb872a91bb8459
Bishal Deb <bishaldeb656@gmail.com>
2019-01-08 16:16:04 +0530

    Delete option added and home page styled.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-var methodOverride  = require("method-override"),
+var expressSanitizer= require("express-sanitizer"),
+    methodOverride  = require("method-override"),
     bodyParser      = require("body-parser"),
     mongoose        = require("mongoose"),
     express         = require("express"),
@@ -10,6 +11,7 @@ app.set("view engine","ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 //MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
     title: String,
@@ -43,6 +45,7 @@ app.get("/blogs/new", function(req, res){
 
 //CREATE POST
 app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -76,6 +79,7 @@ app.get("/blogs/:id/edit", function(req, res){
 
 // UPDATE POST
 app.put("/blogs/:id",function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
         if(err){
             res.redirect("/blogs");
@@ -85,10 +89,20 @@ app.put("/blogs/:id",function(req, res){
     });
 });
 
+// DELETE 
+app.delete("/blogs/:id",function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+});
 
 
 
 
-app.listen(1234, process.env.IP, function(){
+app.listen(1234, function(){
     console.log("Your application is running http://localhost:1234/");
 });
